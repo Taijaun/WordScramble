3676554b331cfe4d352249c3f5114cdec7f238a8
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-02-26 20:09:16 +0000

    Initial tutorial completed

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Taijaun Pitt on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
